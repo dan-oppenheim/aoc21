9abc1b52fad43c3fdb862afe854069b00b3ebd46
Dan <dan@oppenheim.me.uk>
2021-12-01 09:54:11 +0000

    Update path for new input file

--- day1/day1.py
+++ day1/day1.py
@@ -1,5 +1,5 @@
 
-with open('./input.txt') as inputfile:
+with open('day1_input.txt') as inputfile:
     current_reading = int(inputfile.readline())
     larger_count = 0
     for line in inputfile:
