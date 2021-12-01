9ed27f555cb660c3e967d37cf9506b4e2f320602
Dan <dan@oppenheim.me.uk>
2021-12-01 09:27:39 +0000

    Initial commit and day 1, part 1 solution

---
+++ day1/day1.py
@@ -0,0 +1,14 @@
+
+with open('./input.txt') as inputfile:
+    current_reading = int(inputfile.readline())
+    larger_count = 0
+    for line in inputfile:
+        reading = int(line)
+
+        if reading > current_reading:
+            larger_count += 1
+        current_reading = reading
+
+    print(larger_count)
+        
+        
